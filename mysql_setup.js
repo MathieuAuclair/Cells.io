7016a5b2706d1f2d082a1fa4994016abad4d2289
Mathieu Auclair <mathiass12@hotmail.com>
2016-12-05 19:53:11 -0800

    adding html + javascript (client-side) on the node server

--- mysql_setup.js
+++ mysql_setup.js
@@ -1,7 +1,7 @@
-
-var http = require("http");
+var querystring = require('querystring');
 var mysql = require("mysql");
-
+var http = require("http");
+var fs = require("fs");
 
 var pool = mysql.createPool({
 	host: 'localhost',
@@ -12,8 +12,8 @@ var pool = mysql.createPool({
 });
 
 
-//trying to insert values
-
+//insert values
+/*
 pool.getConnection(function(error, conn){
 	var newPlayer = { NOM: 'Winnie', PLAYER_ID: 1 , RADIUS: 8 , X: 50 , Y: 55};
 	conn.query('INSERT INTO PLAYER SET ?', newPlayer, function(error,results){
@@ -23,9 +23,10 @@ pool.getConnection(function(error, conn){
 	  console.log('SUCESSFULL INSERT');
 	});
 });
+*/
 
 //modify the information
-
+/*
 pool.getConnection(function(error, conn){
 	conn.query(
 		"UPDATE PLAYER SET X = ? WHERE PLAYER_ID = ?",
@@ -39,9 +40,10 @@ pool.getConnection(function(error, conn){
 		}
 	);
 });
+*/
 
-// reading information from database
 
+// create server and load files
 pool.getConnection(function(error, conn){
 	var queryString = 'SELECT * FROM PLAYER';
 	conn.query(queryString, function (error,results)
@@ -52,13 +54,32 @@ pool.getConnection(function(error, conn){
 		}
 		else
 		{
-			console.log(results);
+			//console.log(results);
 
-			http.createServer(function(request, response){
-			response.writeHead(200, {'Content-Type' : 'text/plain' });
-			response.write("name is " + JSON.stringify(results[0].NOM));
-			response.end();
+			http.createServer(function(request, response)
+			{
+				
+				
+				function get(fileType, nameFile){ 
+					fs.readFile(nameFile,function (err, data){
+						response.writeHead(200, {'Content-Type': fileType,'Content-Length':data.length});
+						response.write(data);
+						console.log("GET / " + nameFile);
+						response.end();
+					});
+				}
+				
+				
+				
+				get("html","index.html");
+				//get("javascript","app.js"); //doesn't on the side
+				
+				
+				
+				//response.writeHead(200, {'Content-Type' : 'text/html' });
+				//response.write("name is " + JSON.stringify(results[0].NOM)); 
 			}).listen(8080);
+			console.log(" * \n * \n Server is running on localhost \n : 127.0.0.1:8080 \n *"); //launch server display
 		}
 	});
 	conn.release();
