36aa5228a5778a4745112cbc2615770c8ede6933
Mathieu Auclair <mathiass12@hotmail.com>
2016-12-08 12:10:20 -0800

    better code structure

--- mysql_setup.js
+++ mysql_setup.js
@@ -14,39 +14,6 @@ var pool = mysql.createPool({ //giving program acces to database with info pool
 });
 
 
-//function that insert values in database with a different poolConnection
-function insertNewPlayer(idPlayer, nomPlayer)
-{
-	pool.getConnection(function(error, conn){
-		var newPlayer = { NOM: nomPlayer, PLAYER_ID: idPlayer , RADIUS: 8 , X: 50 , Y: 55};
-		conn.query('INSERT INTO PLAYER SET ?', newPlayer, function(error,results){
-		  if(error) 
-			  throw error;
-		  else
-		  console.log('SUCESSFULL INSERT');
-		});
-	});
-}
-
-//modify the information
-function UpdateInfo(id)
-{
-	pool.getConnection(function(error, conn){
-		conn.query(
-			"UPDATE PLAYER SET X = ? WHERE PLAYER_ID = ?",
-			[95, id],
-			function(error, results)
-			{
-				if(error)
-					throw error
-				else
-					console.log("Update info : " + results.changedRows);
-			}
-		);
-	});
-}
-
-
 // create server and load files
 pool.getConnection(function(error, conn){//get acces to database
 	var queryString = "SELECT * FROM PLAYER";
@@ -60,23 +27,18 @@ pool.getConnection(function(error, conn){//get acces to database
 		{	
 			http.createServer(function(request, response) //create a server to interact with server and client
 			{
-				if(request.url=='/index.html' || request.url=='/') 
+				if(request.url=='/index.html' || request.url=='/') //use to do not reapeat twice the code with navigator loading favicon.ico after loading index.html
 				{
-					
-					fs.readFile("index.html",function (err, data){
+					fs.readFile("index.html",function (err, data){ //get index.html
+						//give playerId
+						var id = Math.round(Math.random()*999);
+						console.log(" / \n / \n / \nsession id is : " + id);
+						//load html
 						response.writeHead(200, {'Content-Type': "html",'Content-Length':data.length});
 						response.write(data);
 						console.log("GET / " + request.url);
-						response.end(dropPlayer(id));
+						response.end(insertNewPlayer(id, "Mathieu"));
 					});
-					//give a id to player
-						//need to check if id exist when ur going to know thred
-					var id = Math.round(Math.random()*999);
-					 
-					console.log("session id is : " + id);
-					insertNewPlayer(id, "Mathieu");
-					loadInfo(id);
-						
 				}
 			}).listen(8080);
 			console.log(" * \n * \n Server is running on localhost \n : 127.0.0.1:8080 \n *"); //launch server display
@@ -85,25 +47,23 @@ pool.getConnection(function(error, conn){//get acces to database
 	conn.release();
 });
 
-//function that drop player from database
-
-function dropPlayer(id)
+//function that insert values in database with a different poolConnection
+function insertNewPlayer(idPlayer, nomPlayer)
 {
 	pool.getConnection(function(error, conn){
-		conn.query(
-			"DELETE FROM PLAYER WHERE PLAYER_ID = ?",
-			[id],
-			function(error, results)
+		var newPlayer = { NOM: nomPlayer, PLAYER_ID: idPlayer , RADIUS: 8 , X: 50 , Y: 55};
+		conn.query('INSERT INTO PLAYER SET ?', newPlayer, function(error,results)
 			{
-				if(error)
-					throw error
-				else
-					console.log("data drop successfully \n / \n / \n /");
+			  if(error) 
+				  throw error;
+			  else
+			  console.log('SUCESSFULL INSERT');
+
 			}
 		);
+		conn.release(loadInfo(idPlayer));
 	});
 }
-
 
 //function to load data with player id
 function loadInfo(id)
@@ -124,6 +84,53 @@ function loadInfo(id)
 				return false;
 			}
 		});
+		conn.release();
 	});
 }
+
+//modify the information
+function UpdateInfo(id)
+{
+	pool.getConnection(function(error, conn){
+		conn.query
+		(
+			"UPDATE PLAYER SET X = ? WHERE PLAYER_ID = ?",
+			[95, id],
+			function(error, results)
+			{
+				if(error)
+					throw error
+				else
+					console.log("Update info : " + results.changedRows);
+			}
+		);
+		conn.release();
+	});
+}	
+
+	
+//function that drop player from database
+
+function dropPlayer(id)
+{
+	pool.getConnection(function(error, conn){
+		conn.query
+		(
+			"DELETE FROM PLAYER WHERE PLAYER_ID = ?",
+			[id],
+			function(error, results)
+			{
+				if(error)
+					throw error
+				else
+					console.log("data drop successfully! ");
+			}
+		);
+		conn.release();
+	});
+}
+
+
+
+
 
